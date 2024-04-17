4d198bbfc1eddb0f5f09b541234b022da956e896
Juan Diego Silva Castillo <juan.silva.castillo@udla.edu.ec>
2024-04-17 11:06:59 -0500

    Agregar archivos de proyecto.

---
+++ Data/JuanDiego_TallerMVC2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JuanDiego_TallerMVC2.Models;
+
+namespace JuanDiego_TallerMVC2.Data
+{
+    public class JuanDiego_TallerMVC2Context : DbContext
+    {
+        public JuanDiego_TallerMVC2Context (DbContextOptions<JuanDiego_TallerMVC2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JuanDiego_TallerMVC2.Models.Burger> Burger { get; set; } = default!;
+    }
+}
